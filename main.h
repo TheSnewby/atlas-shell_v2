3e2b9758a9e4d74cac5212827db3645305f66745
Deshon Stout <46934055+DeshonS@users.noreply.github.com>
2025-02-12 20:12:36 -0600

    added declaration of ifCmdEcho

--- main.h
+++ main.h
@@ -95,6 +95,7 @@ char *getHostname(void);
 char* _strstr(char *sentence, char *word);
 int RightDirect(char **tokens);
 int StreamDirect(char **tokens);
+int ifCmdEcho(char **tokens);
 size_t _strcspn(const char *str1, const char *str2);
 
 /* --- Utility Functions --- */
