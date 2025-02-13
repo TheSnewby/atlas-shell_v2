341e5392561f13bdcdd76b097d4597a92e4d647d
Deshon Stout <46934055+DeshonS@users.noreply.github.com>
2025-02-13 10:13:34 -0600

    Update main.h

--- main.h
+++ main.h
@@ -95,7 +95,6 @@ char *getHostname(void);
 char* _strstr(char *sentence, char *word);
 int RightDirect(char *line);
 int StreamDirect(char **tokens);
-int ifCmdEcho(char **tokens);
 size_t _strcspn(const char *str1, const char *str2);
 
 /* --- Utility Functions --- */
