a66c73273eb275e688f2a49ffd52f267698cca6e
Deshon Stout <46934055+DeshonS@users.noreply.github.com>
2025-02-13 09:57:46 -0600

    Update executor.c

--- executor.c
+++ executor.c
@@ -200,7 +200,7 @@ int isCommand(const char *fp)
 	path_t *head = NULL;
 	char *temp_path = NULL;
 
-	if (_strchr(fp, '/') && !access(fp, F_OK)) /* checks if path already */
+	if (_strchr(fp, "/") && !access(fp, F_OK)) /* checks if path already */
 		return (1);
         // return (access(fp, F_OK | X_OK) == 0 ? 1 : 0);
 
