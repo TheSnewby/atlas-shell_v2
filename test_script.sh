76ede457f989f2faff89fad35f17229107fdd14e
TheSnewby <stephen.j.newby@gmail.com>
2025-02-19 20:18:14 -0600

    Revert "adding more test cases in test_script"
    This reverts commit 88848ccd7e9988df1032f70967aedeb162a3f22a.

--- test_script.sh
+++ test_script.sh
@@ -22,17 +22,10 @@ cd /root
     
        
 fakeCommand
-/bin/ls
-"
-
-EXIT_COMMAND1="exit"
-EXIT_COMMAND2="exit 5"
-EXIT_COMMAND3="exit -5"
-EXIT_COMMAND4="exit string"
+/bin/ls"
 
 echo "===== Running in Non-Interactive Mode ====="
 echo "$COMMANDS" | $VALGRIND_CMD
-echo
 
 echo
 echo "===== Running in Interactive Mode ====="
@@ -43,23 +36,6 @@ echo "$COMMANDS" > "$TMPFILE"
 
 # Run valgrind interactively using input redirection
 $VALGRIND_CMD < "$TMPFILE"
-echo
-
-echo "===== Non-Interactive Mode exit no param ====="
-echo "$EXIT_COMMAND1" | $VALGRIND_CMD
-echo
-
-echo "===== Non-Interactive Mode exit 5 ====="
-echo "$EXIT_COMMAND2" | $VALGRIND_CMD
-echo
-
-echo "===== Non-Interactive Mode exit -5 ====="
-echo "$EXIT_COMMAND3" | $VALGRIND_CMD
-echo
-
-echo "===== Non-Interactive Mode exit string ====="
-echo "$EXIT_COMMAND4" | $VALGRIND_CMD
-echo
 
 # Clean up
 rm "$TMPFILE"
