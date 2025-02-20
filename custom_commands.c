379a6ec16b7c6f702156ad122987adf687f5a7ee
Deshon Stout <46934055+DeshonS@users.noreply.github.com>
2025-02-20 09:19:57 -0600

    Update custom_commands.c / Removed Echos

--- custom_commands.c
+++ custom_commands.c
@@ -39,15 +39,6 @@ int customCmd(char **tokens, int interactive, char *input)
 	if (ifRtn)
 		return (ifRtn);
 
-	/* ----------------- custom command "echo" ----------------- */
-	if (ifCmdEcho(tokens) == 1)
-	{
-		for (int i = 0; tokens[i] != NULL; i++)
-		{
-			if (_strcmp(tokens[i], ">") == 0)
-				return RightDirect(tokens);
-		}
-	}
 	return (0); /* indicate that the input is not a custom command */
 }
 
@@ -365,13 +356,3 @@ int RightDirect(char **tokens)
     }
     return 1;
 }
-
-int ifCmdEcho(char **tokens)
-{
-	if (tokens[0] != NULL && (_strcmp(tokens[0], "echo") == 0))
-	{
-		return (1);
-	}
-	return (0);
-}
-
